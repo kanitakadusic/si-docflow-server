84bc5b48f286eef230c1d5c0949575b768269646
Kanita Kadusic <kkadusic2@etf.unsa.ba>
2025-05-04 23:25:55 +0200

    Handle multi-line fields

--- src/database/models/documentLayout.model.ts
+++ src/database/models/documentLayout.model.ts
@@ -4,6 +4,7 @@ export interface IField {
     name: string;
     upper_left: [number, number];
     lower_right: [number, number];
+    is_multiline: boolean;
 }
 
 interface IDocumentLayout {
--- src/services/ocr.service.ts
+++ src/services/ocr.service.ts
@@ -56,6 +56,10 @@ export class OcrService {
             // <- debug
 
             const ocrResult = await engine.extract(cropped);
+            if (!field.is_multiline) {
+                ocrResult.text = ocrResult.text.replace(/\n/g, '');
+            }
+
             result.push({ field, ocrResult });
         }
 
