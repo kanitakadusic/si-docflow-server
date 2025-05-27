9b07e098c19fce72b71f5b0600ccdeb77f8c84ae
Kanita Kadusic <kkadusic2@etf.unsa.ba>
2025-05-27 20:56:55 +0200

    API docs

--- src/routes/documentType.routes.ts
+++ src/routes/documentType.routes.ts
@@ -5,6 +5,32 @@ import { DocumentTypeController } from '../controllers/documentType.controller.j
 const router = express.Router();
 const documentTypeController = new DocumentTypeController();
 
+/**
+ * @swagger
+ * /document/types:
+ *   get:
+ *     summary: Fetch all document types
+ *     tags:
+ *       - Document Type
+ *     responses:
+ *       200:
+ *         description: Document types have been successfully fetched
+ *         content:
+ *           application/json:
+ *             example:
+ *               data:
+ *                 - id: 1
+ *                   name: "Employment Verification Form"
+ *                   description: "Confirms a person's job status and details."
+ *                   document_layout_id: 1
+ *               message: Document types have been successfully fetched
+ *       500:
+ *         description: Server error while fetching document types
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: Server error while fetching document types
+ */
 router.get('/types', documentTypeController.getAll.bind(documentTypeController));
 
 export default router;
